8537ae122d13fac170ee0ef92ae3b52e31d2980d
unknown <diasdeoliveira12@gmail.com>
2020-12-22 15:02:28 -0300

    feat: validate email with domain larger than 255 char

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,4 +1,3 @@
-  
 export class Email {
     static validate (email: string): boolean {
       if (!email) {
@@ -9,9 +8,12 @@ export class Email {
         return false
       }
   
-      const [local] = email.split('@')
+      const [local, domain] = email.split('@')
   
       if (local.length > 64) {
+        return false
+      }
+      if (domain.length > 255) {
         return false
       }
       return true
