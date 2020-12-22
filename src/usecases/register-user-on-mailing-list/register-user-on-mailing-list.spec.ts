36706332f0c19dbd2bd465b8f85e5333b4b5e3e0
unknown <diasdeoliveira12@gmail.com>
2020-12-22 14:53:20 -0300

    test: register on mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,18 @@
+
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', async () => {
+    const user: UserData[] = []
+    console.log(user)
+    // const repo: UserRepository = new InMemoryRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //   repo
+    // )
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await usecase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
