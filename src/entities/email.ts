146ffa7e4a08ffeae23b5198399eb5f7edef59e7
unknown <diasdeoliveira12@gmail.com>
2020-12-22 15:10:42 -0300

    feat: validate email with local part with invalid char

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,10 +1,16 @@
 export class Email {
     static validate (email: string): boolean {
+      const emailRegez = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-z0-9](-*\.?[a-zA-z0-9])*\.[a-zA-z](-?[a-zA-z0-9])+$/
+  
       if (!email) {
         return false
       }
   
       if (email.length > 320) {
+        return false
+      }
+  
+      if (!emailRegez.test(email)) {
         return false
       }
   
