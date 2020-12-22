7e86c612b7295a90419fc9e2d6e84d18d026bb3a
unknown <diasdeoliveira12@gmail.com>
2020-12-22 14:57:23 -0300

    feat: validate local part larger thean 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,7 +1,12 @@
-
 export class Email {
     static validate (email: string): boolean {
       if (!email) {
+        return false
+      }
+  
+      const [local] = email.split('@')
+  
+      if (local.length > 64) {
         return false
       }
       return true
