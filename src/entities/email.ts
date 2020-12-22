529b137811ab2c2f3f1ee53512cf4f062dc43065
unknown <diasdeoliveira12@gmail.com>
2020-12-22 15:09:36 -0300

    feat: validate email with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -13,7 +13,12 @@ export class Email {
       if (local.length > 64 || local.length === 0) {
         return false
       }
-      if (domain.length > 255  || domain.length === 0) {
+      if (domain.length > 255 || domain.length === 0) {
+        return false
+      }
+  
+      const domainParts = domain.split('.')
+      if (domainParts.some(part => part.length > 63)) {
         return false
       }
       return true
