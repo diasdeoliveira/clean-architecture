cee0e5a4b619a8538e08020c848a8c7413373488
unknown <diasdeoliveira12@gmail.com>
2021-01-14 19:41:39 -0300

    test: verify name while creating user

--- src/entities/user-spec.ts
+++ src/entities/user-spec.ts
@@ -1,7 +1,7 @@
-
 import { User } from './user'
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 
 describe('User domain class', () => {
   it('Should not create user with invalid e-mail address', () => {
@@ -9,5 +9,19 @@ describe('User domain class', () => {
     const error = User.create({ name: 'any_name', email: invalidEmail })
 
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  it('Should not create user with invalid name (too few characters)', () => {
+    const invalidName = 'O    '
+    const error = User.create({ name: invalidName, email: 'any@gmail.com' })
+
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  it('Should not create user with invalid name (too many characters)', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@gmail.com' })
+
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
