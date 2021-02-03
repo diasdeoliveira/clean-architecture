33b5f82b53c3fa9a2939b7db327c4ff5b354f0e6
unknown <diasdeoliveira12@gmail.com>
2021-02-03 19:09:01 -0300

    refactor: introduce UseCase interface

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -1,16 +1,17 @@
-import { InvalidEmailError, InvalidNameError } from '../../entities/errors'
-import { User, UserData } from '../../entities'
-import { Either, left, right } from '../../shared'
-import { UserRepository } from './ports'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { User, UserData } from '@/entities'
+import { Either, left, right } from '@/shared'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { UseCase } from '@/usecases/ports'
 
-export class RegisterUserOnMailingList {
+export class RegisterUserOnMailingList implements UseCase {
   private readonly userRepo: UserRepository
 
   constructor (userRepo: UserRepository) {
     this.userRepo = userRepo
   }
 
-  public async registerUserOnMailingList (
+  public async perform (
     request: UserData
   ): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
     const userOrError: Either<
