51f771828313dd0ac9ed745ad733751a1a37c336
unknown <diasdeoliveira12@gmail.com>
2021-02-10 20:34:02 -0300

    feat: apply cors middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,9 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
+
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
