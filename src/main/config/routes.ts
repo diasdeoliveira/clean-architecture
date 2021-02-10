744e142584e612626f91b43ad97306865e452bad
unknown <diasdeoliveira12@gmail.com>
2021-02-10 20:48:23 -0300

    feat: config register route

---
+++ src/main/config/routes.ts
@@ -0,0 +1,12 @@
+/* eslint-disable node/no-path-concat */
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import path from 'path'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  readdirSync(path.join(__dirname, 'routes')).map(async file => {
+    ;(await import(path.join(__dirname, 'routes', file))).default(router)
+  })
+}
